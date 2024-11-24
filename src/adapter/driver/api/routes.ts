e54ae94e90a23e50e7aa8ac85ba95e5a08ebf699
Lucas Santos <lucasa95santos@gmail.com>
2024-11-24 13:44:14 -0300

    feat: add initial config

---
+++ src/adapter/driver/api/routes.ts
@@ -0,0 +1,138 @@
+import {
+  Request as ExpressRequest,
+  Response as ExpressResponse,
+  NextFunction as ExpressNextFuction,
+} from 'express'
+import { HttpMethod, IRouteProps } from './types/http-server'
+
+export const healthRoutes: IRouteProps[] = [
+  {
+    resource: '/health',
+    method: HttpMethod.GET,
+    middleware: (
+      _request: ExpressRequest,
+      _response: ExpressResponse,
+      next: ExpressNextFuction,
+    ) => next(),
+    handler: 'check',
+  },
+]
+
+export const customerRoutes: IRouteProps[] = [
+  {
+    resource: '/clientes',
+    method: HttpMethod.POST,
+    middleware: (
+      _request: ExpressRequest,
+      _response: ExpressResponse,
+      next: ExpressNextFuction,
+    ) => next(),
+    handler: 'createCustomer',
+  },
+  {
+    resource: '/clientes/:document',
+    method: HttpMethod.GET,
+    middleware: (
+      _request: ExpressRequest,
+      _response: ExpressResponse,
+      next: ExpressNextFuction,
+    ) => next(),
+    handler: 'getCustomerByDocument',
+  },
+]
+
+export const productRoutes: IRouteProps[] = [
+  {
+    resource: '/produtos',
+    method: HttpMethod.POST,
+    middleware: (
+      _request: ExpressRequest,
+      _response: ExpressResponse,
+      next: ExpressNextFuction,
+    ) => next(),
+    handler: 'createProduct',
+  },
+  {
+    resource: '/produtos/:id',
+    method: HttpMethod.PUT,
+    middleware: (
+      _request: ExpressRequest,
+      _response: ExpressResponse,
+      next: ExpressNextFuction,
+    ) => next(),
+    handler: 'updateProduct',
+  },
+  {
+    resource: '/produtos',
+    method: HttpMethod.GET,
+    middleware: (
+      _request: ExpressRequest,
+      _response: ExpressResponse,
+      next: ExpressNextFuction,
+    ) => next(),
+    handler: 'searchProducts',
+  },
+  {
+    resource: '/produtos/:id',
+    method: HttpMethod.DELETE,
+    middleware: (
+      _request: ExpressRequest,
+      _response: ExpressResponse,
+      next: ExpressNextFuction,
+    ) => next(),
+    handler: 'removeProduct',
+  },
+]
+
+export const orderRoutes: IRouteProps[] = [
+  {
+    resource: '/checkout',
+    method: HttpMethod.POST,
+    middleware: (
+      _request: ExpressRequest,
+      _response: ExpressResponse,
+      next: ExpressNextFuction,
+    ) => next(),
+    handler: 'makeCheckout',
+  },
+  {
+    resource: '/pedidos',
+    method: HttpMethod.GET,
+    middleware: (
+      _request: ExpressRequest,
+      _response: ExpressResponse,
+      next: ExpressNextFuction,
+    ) => next(),
+    handler: 'searchOrders',
+  },
+  {
+    resource: '/pedidos/:id/atualizar-status',
+    method: HttpMethod.PUT,
+    middleware: (
+      _request: ExpressRequest,
+      _response: ExpressResponse,
+      next: ExpressNextFuction,
+    ) => next(),
+    handler: 'updateOrderStatus',
+  },
+  {
+    resource: '/pedidos/:id/pagamento',
+    method: HttpMethod.GET,
+    middleware: (
+      _request: ExpressRequest,
+      _response: ExpressResponse,
+      next: ExpressNextFuction,
+    ) => next(),
+    handler: 'getOrderPayment',
+  },
+  {
+    resource: '/pedidos/webhook-pagamento',
+    method: HttpMethod.POST,
+    middleware: (
+      _request: ExpressRequest,
+      _response: ExpressResponse,
+      next: ExpressNextFuction,
+    ) => next(),
+    handler: 'listenOrderPayment',
+  },
+]
---
+++ src/bootstrap.ts
@@ -0,0 +1,90 @@
+import { globalEnvs } from '@config/envs/global'
+import {
+  CreateCustomerUseCase,
+  CreateProductUseCase,
+  GetCustomerByDocumentUseCase,
+  GetOrderPaymentUseCase,
+  ListenOrderPaymentUseCase,
+  MakeCheckoutUseCase,
+  RemoveProductUseCase,
+  SearchOrdersUseCase,
+  SearchProductsUseCase,
+  UpdateOrderStatusUseCase,
+  UpdateProductUseCase,
+} from '@core/application/use-cases'
+import {
+  HealthController,
+  CustomerController,
+  OrderController,
+  ProductController,
+} from '@adapter/driver/api/controllers'
+import { ExpressHttpServerAdapter } from '@adapter/driver/api/express-server.adapter'
+import { IHttpServer } from '@adapter/driver/api/types/http-server'
+import { PostgresConnectionAdapter } from '@adapter/driven/database/postgres-connection.adapter'
+import { CustomerRepository } from '@adapter/driven/database/repositories'
+import { ProductRepository } from '@adapter/driven/database/repositories/product.repository'
+import { PaymentRepository } from '@adapter/driven/database/repositories/payment.repository'
+import { OrderRepository } from '@adapter/driven/database/repositories/order.repository'
+import { MercadoPagoAdapter } from '@adapter/driven/payment-solution/mercado-pago.adapter'
+import { HttpClientAdapter } from '@adapter/driven/http/http-client.adapter'
+
+const postgresConnectionAdapter = new PostgresConnectionAdapter()
+const httpClientAdapter = new HttpClientAdapter(
+  globalEnvs.paymentSolution.baseUrl,
+)
+const paymentSolution = new MercadoPagoAdapter(httpClientAdapter)
+// repositories
+const customerRepository = new CustomerRepository(postgresConnectionAdapter)
+const productRepository = new ProductRepository(postgresConnectionAdapter)
+const paymentRepository = new PaymentRepository(postgresConnectionAdapter)
+const orderRepository = new OrderRepository(postgresConnectionAdapter)
+// useCases
+const createCustomerUseCase = new CreateCustomerUseCase(customerRepository)
+const getCustomerByDocumentUseCase = new GetCustomerByDocumentUseCase(
+  customerRepository,
+)
+const createProductUseCase = new CreateProductUseCase(productRepository)
+const updateProductUseCase = new UpdateProductUseCase(productRepository)
+const searchProductsUseCase = new SearchProductsUseCase(productRepository)
+const removeProductUseCase = new RemoveProductUseCase(productRepository)
+const makeCheckoutUseCase = new MakeCheckoutUseCase(
+  customerRepository,
+  productRepository,
+  paymentRepository,
+  orderRepository,
+  paymentSolution,
+)
+const searchOrdersUseCase = new SearchOrdersUseCase(orderRepository)
+const updateOrderStatusUseCase = new UpdateOrderStatusUseCase(orderRepository)
+const getOrderPaymentUseCase = new GetOrderPaymentUseCase(paymentRepository)
+const listenOrderPaymentUseCase = new ListenOrderPaymentUseCase(
+  orderRepository,
+  paymentRepository,
+  paymentSolution,
+)
+// controllers
+const healthController = new HealthController()
+const customerController = new CustomerController(
+  createCustomerUseCase,
+  getCustomerByDocumentUseCase,
+)
+const productController = new ProductController(
+  createProductUseCase,
+  updateProductUseCase,
+  searchProductsUseCase,
+  removeProductUseCase,
+)
+const orderController = new OrderController(
+  makeCheckoutUseCase,
+  searchOrdersUseCase,
+  updateOrderStatusUseCase,
+  getOrderPaymentUseCase,
+  listenOrderPaymentUseCase,
+)
+const server: IHttpServer = new ExpressHttpServerAdapter(
+  healthController,
+  customerController,
+  productController,
+  orderController,
+)
+server.run(globalEnvs.api.serverPort)

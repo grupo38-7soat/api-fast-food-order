37de2743570b033510ed0035bef8eb59e6e705fb
Lucas Santos <lucasa95santos@gmail.com>
2024-11-26 21:28:35 -0300

    test: add tests for all product use cases

--- test/bootstrap.test.ts
+++
@@ -1,3 +0,0 @@
-test('should return true if 1 to be 1', () => {
-  expect(1).toBe(1)
-})
---
+++ test/unit/core/application/use-cases/product/remove-product.use-case.spec.ts
@@ -0,0 +1,80 @@
+/* eslint-disable jest/no-focused-tests */
+import { RemoveProductUseCase } from '@core/application/use-cases'
+import { Category, Product } from '@core/domain/entities'
+import { IProductRepository } from '@core/domain/repositories'
+
+describe('RemoveProductUseCase', () => {
+  let productRepositoryMock: IProductRepository
+  let sut: RemoveProductUseCase
+
+  beforeAll(() => {
+    productRepositoryMock = {
+      saveProduct: jest.fn(),
+      findProductByParam: jest.fn(),
+      findAllProducts: jest.fn(),
+      removeProduct: jest.fn(),
+    }
+    sut = new RemoveProductUseCase(productRepositoryMock)
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  afterAll(() => {
+    jest.resetAllMocks()
+  })
+
+  it('should be defined', () => {
+    expect(sut).toBeDefined()
+  })
+
+  describe('execute method', () => {
+    it('should throws if id is empty', async () => {
+      const input = {
+        id: 0,
+      }
+      await expect(sut.execute(input)).rejects.toThrow(
+        'O id deve ser informado',
+      )
+      expect(productRepositoryMock.findProductByParam).not.toHaveBeenCalled()
+      expect(productRepositoryMock.removeProduct).not.toHaveBeenCalled()
+    })
+
+    it('should throws if any product was found', async () => {
+      jest
+        .spyOn(productRepositoryMock, 'findProductByParam')
+        .mockResolvedValueOnce(null)
+      const input = {
+        id: 1,
+      }
+      await expect(sut.execute(input)).rejects.toThrow('Produto não encontrado')
+      expect(productRepositoryMock.findProductByParam).toHaveBeenCalledTimes(1)
+      expect(productRepositoryMock.findProductByParam).toHaveBeenCalledWith(
+        'id',
+        input.id,
+      )
+      expect(productRepositoryMock.removeProduct).not.toHaveBeenCalled()
+    })
+
+    it('should remove a product', async () => {
+      jest
+        .spyOn(productRepositoryMock, 'findProductByParam')
+        .mockResolvedValueOnce(
+          new Product('some_info', 'some_info', 1, Category.LANCHE, [
+            'some_info',
+          ]),
+        )
+      const input = {
+        id: 1,
+      }
+      await sut.execute(input)
+      expect(productRepositoryMock.findProductByParam).toHaveBeenCalledTimes(1)
+      expect(productRepositoryMock.findProductByParam).toHaveBeenCalledWith(
+        'id',
+        input.id,
+      )
+      expect(productRepositoryMock.removeProduct).toHaveBeenCalledWith(input.id)
+    })
+  })
+})
